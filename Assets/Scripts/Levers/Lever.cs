ca3379310ba6a64adc491ea3013e4603dc17f0e8
Boris Vezmar <boris.vezmar@fer.hr>
2017-12-12 14:43:53 +0100

    Initial levers feature commit

---
+++ Assets/Scripts/Levers/Lever.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour {
+
+	public int leverValue;
+
+	public delegate void toggleHandler(int valueChange);
+	public static event toggleHandler onLeverToggle;
+
+	private bool turnedOn = false;
+
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+		
+	public void toggleLever() {
+		turnedOn = !turnedOn;
+		 
+		int valueChange;
+		if (turnedOn) {
+			valueChange = leverValue;
+		} else {
+			valueChange = -leverValue;
+		}
+
+		// Is someone subscribed to lever toggling?
+		if (onLeverToggle != null) {
+			onLeverToggle (valueChange);
+		}
+
+	}
+
+	public bool isTurnedOn() {
+		return turnedOn;
+	}
+		
+}
