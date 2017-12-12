be14426f4f71322df1c1fac9091426a1de896aaa
Boris Vezmar <boris.vezmar@fer.hr>
2017-12-12 14:43:53 +0100

    Initial levers feature commit

---
+++ Assets/Scripts/Levers/LeverSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverSystem : MonoBehaviour {
+
+	public int targetLeverValue;
+
+	private int currentLeverValue;
+	private bool systemEnabled;
+
+	void myOnLeverToggle(int valueChange) {
+		currentLeverValue += valueChange;
+
+		if (currentLeverValue == targetLeverValue) {
+			systemEnabled = true;
+		} else {
+			systemEnabled = false;
+		}
+	}
+
+	void OnEnable() {
+		Lever.onLeverToggle += myOnLeverToggle;
+	}
+
+	void OnDisable() {
+		Lever.onLeverToggle -= myOnLeverToggle;
+	}
+
+	void Start () {
+		int currentLeverValue = 0;
+		systemEnabled = false;
+	}
+	
+	void Update () {
+		
+	}
+		
+}
