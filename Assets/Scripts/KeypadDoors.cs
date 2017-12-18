e5144fa7998dfc70b3a3fe6e7da3fcc447759991
Ivica Jezernik <jezernik.ivica@gmail.com>
2017-12-18 17:09:08 +0100

    some more work on keypad doors

--- Assets/Scripts/KeypadDoors.cs
+++ Assets/Scripts/KeypadDoors.cs
@@ -1,22 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeypadDoors : MonoBehaviour {
-    public Object Door;
-    public string Key;
+    public Component Door;
+    public int Key;
+    public Canvas keypadCanvas;
+    private string enteredKey;
+    public Text displayKey;
 
 	// Use this for initialization
 	void Start () {
-		
+        enteredKey = "";
 	}
+
+    void ActivateKeypad()
+    {
+        keypadCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void DeactivateKeypad()
+    {
+        keypadCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
 
     public void OnEnterPress()
     {
-
+        if (Key.ToString() == enteredKey){
+            Door.GetComponent<Animator>().SetBool("open", true);
+            DeactivateKeypad();
+        }
+        else {
+            enteredKey = "";
+        }
     }	
+
 	// Update is called once per frame
 	void Update () {
-		
+       //displayKey.text = enteredKey.ToString();
 	}
 }
