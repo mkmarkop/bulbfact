f9c65e5bbe64be4f47f139f50bf94e06c34f051b
Boris Vezmar <boris.vezmar@fer.hr>
2018-01-05 21:47:26 +0100

    Initial interactive objects (electricity) commit

---
+++ Assets/Scripts/Interactive_objects(electricity)/ChargeableObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float requiredCharge;
+
+    private float _currentCharge;
+    private float _maxCharge;
+    private bool _objectCharged;
+	
+	void Start () {
+        _currentCharge = 0.0f;
+        _maxCharge = 2000.0f;
+        _objectCharged = false;
+	}
+	
+	void Update () {
+		if(_currentCharge >= requiredCharge)
+        {
+            _objectCharged = true;
+        }
+	}
+
+    public void charge(float receivedCharge)
+    {
+        _currentCharge += receivedCharge;
+
+        if(_currentCharge < 0.0f)
+        {
+            _currentCharge = 0;
+        } else if(_currentCharge > _maxCharge)
+        {
+            _currentCharge = _maxCharge;
+        }
+    }
+
+    public bool isCharged()
+    {
+        return _objectCharged;
+    }
+
+}
---
+++ Assets/Scripts/Interactive_objects(electricity)/TelevisionDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public string password;
+
+    private ChargeableObject _charge;
+    private bool _displayOn;
+
+	void Start () {
+        _charge = GetComponent<ChargeableObject>();
+        _displayOn = false;
+	}
+	
+	void Update () {
+		if(!_displayOn)
+        {
+            if(_charge.isCharged())
+            {
+                displayPassword();
+            }
+        }
+	}
+
+    private void displayPassword()
+    {
+
+    }
+}
