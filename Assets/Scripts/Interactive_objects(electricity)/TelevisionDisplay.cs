78d3c8f0a59e5f92e4c5979cfa1fd24b4a345969
Boris Vezmar <boris.vezmar@fer.hr>
2018-01-08 17:10:26 +0100

    Television script done

--- Assets/Scripts/Interactive_objects(electricity)/TelevisionDisplay.cs
+++ Assets/Scripts/Interactive_objects(electricity)/TelevisionDisplay.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class TelevisionDisplay : ChargeableObject {
 
+    // The password to display once television is charged
     public string password;
+    
+    // The Canvas UI Text used to display the password
+    public Text displayText;
 
     private bool _displayOn;
 
 	void Start () {
         base.Start();
         _displayOn = false;
-	}
+        displayText.enabled = false;
+        displayText.text = password;
+        displayText.color = Color.green;
+        displayText.fontStyle = FontStyle.Bold;
+        displayText.fontSize = 20;
+    }
 	
 	void Update () {
         base.Update();
@@ -28,6 +38,7 @@ public class TelevisionDisplay : ChargeableObject {
     //turn on display and show password
     override public void executeAction()
     {
-
+        _displayOn = true;
+        displayText.enabled = true;
     }
 }
