7aaf411c71cc1f32e3d1a04bd5a34a9d84fdef29
Ivica Jezernik <jezernik.ivica@gmail.com>
2018-01-08 13:48:31 +0100

    merged interactive objects

--- Assets/Scripts/Discharger.cs
+++ Assets/Scripts/Discharger.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ChargeableObject))]
 public class Discharger : MonoBehaviour {
+    public ChargeableObject ChargeableObject;
+    private Object _player;
 
 	// Use this for initialization
 	void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
 		
 	}
 	
