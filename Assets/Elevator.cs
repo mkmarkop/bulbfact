5e6b7e709c52e5bd98bd5d9b4d1b15b500a6045e
Ivica Jezernik <jezernik.ivica@gmail.com>
2018-01-23 11:33:51 +0100

    otvaranje lifta bez interactable

---
+++ Assets/Elevator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+    private Animator _animator;
+    private BoxCollider _boxCollider;
+    internal bool _open;
+
+    // Use this for initialization
+    void Start () {
+        _animator = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider>();
+        _open = false;
+    }
+
+    protected void Open()
+    {
+        _open = true;
+        _animator.SetBool("open", true);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player")) { return; }
+        if (Input.GetButtonDown("Activate"))
+        {
+            if (isOpen()) { return; }
+            Open();
+        }
+    }
+
+    bool isOpen()
+    {
+        return _open;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
