3ac44bb6b7ec1df7c2b98e95c91f9f2f18792292
Marko Pranjić <marko.pranjic@fer.hr>
2018-01-23 17:43:43 +0100

    Camera finally implemented

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Transform _camTransform;
+    public Transform lookAt;
+	
+	void Start () {
+        _camTransform = transform;
+	}
+
+	void LateUpdate () {
+        _camTransform.position = new Vector3(_camTransform.position.x,
+            _camTransform.position.y, lookAt.position.z);
+	}
+}
