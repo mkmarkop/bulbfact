7b3dab688e2520a0792fb386cbb84de2de0e26bc
Marko Pranjić <marko.pranjic@fer.hr>
2018-01-24 12:15:09 +0100

    Reconfigure build settings

--- Assets/Scripts/Items/InventoryItem.cs
+++ Assets/Scripts/Items/InventoryItem.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using UnityEngine;
+
+#if UNITY_EDITOR
 using UnityEditor;
 
 [CreateAssetMenu(fileName = "InventoryItem.asset", menuName = "Inventory/Item")]
+#endif
 public class InventoryItem : ScriptableObject {
 
 	public Sprite itemIcon;
